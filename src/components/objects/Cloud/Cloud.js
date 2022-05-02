c7e195831a4e6514733e817960e3197b525df440
Julia Ruskin <juliaruskin27@gmail.com>
2022-05-02 17:15:56 -0400

    clouds

--- src/components/objects/Cloud/Cloud.js
+++ src/components/objects/Cloud/Cloud.js
@@ -2,24 +2,41 @@ import { Group, Mesh } from 'three';
 import * as THREE from 'three';
 import MODEL from './scene.gltf';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { MTLLoader } from 'three/examples/jsm/loaders/MTLLoader.js';
+import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader.js';
+
+import OBJ from './cloud.obj';
 class Cloud extends Group {
     addCloud() {
-        const loader = new GLTFLoader();
-        loader.load(
-            MODEL,
-            (gltf) => {
-                var model = gltf.scene;
-                var newMaterial = new THREE.MeshStandardMaterial({color: 0xffffff, opacity: 0.7});
+        var scene = this;
+        var objLoader = new OBJLoader();
+  objLoader.load(OBJ, function(model) {
+    var newMaterial = new THREE.MeshStandardMaterial({color: 0xffffff, opacity: 0.7});
                 model.traverse((o) => {
                 if (o.isMesh) o.material = newMaterial;
                 });
                 model.scale.set(Math.random() * 0.5 + 0.25, Math.random() * 0.5 + 0.25, Math.random() * 0.5 + 0.25);
                 model.rotation.y = Math.random() * Math.PI / 4 - Math.PI / 2;
-                model.position.x = this.state.x + 20;
+                model.position.x = scene.state.x + 20;
                 model.position.y = Math.random() * 2 - 1;
-                this.add(model);
-            }
-        );
+                scene.add(model);
+  });
+        // const loader = new GLTFLoader();
+        // loader.load(
+        //     MODEL,
+        //     (gltf) => {
+        //         var model = gltf.scene;
+        //         var newMaterial = new THREE.MeshStandardMaterial({color: 0xffffff, opacity: 0.7});
+        //         model.traverse((o) => {
+        //         if (o.isMesh) o.material = newMaterial;
+        //         });
+        //         model.scale.set(Math.random() * 0.5 + 0.25, Math.random() * 0.5 + 0.25, Math.random() * 0.5 + 0.25);
+        //         model.rotation.y = Math.random() * Math.PI / 4 - Math.PI / 2;
+        //         model.position.x = this.state.x + 20;
+        //         model.position.y = Math.random() * 2 - 1;
+        //         this.add(model);
+        //     }
+        // );
     }
     constructor(parent) {
         // Call parent Group() constructor
